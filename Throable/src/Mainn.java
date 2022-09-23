da2f25d95e3a1b945a91a2bd90300702046a657e
sandy99405 <55664068+sandy99405@users.noreply.github.com>
2022-09-23 13:25:26 +0530

    Update Mainn.java

--- Throable/src/Mainn.java
+++ Throable/src/Mainn.java
@@ -3,7 +3,7 @@ public class Mainn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println();
+    System.out.println("hello world");
     
     
     
